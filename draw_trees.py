a1c85f7c5cf60017a92d7f205665434c6df21e8e
Richard Halstead <rickynumber24@gmail.com>
2025-02-26 14:55:59 -0500

    Split local dev code out from draw_trees.py

--- draw_trees.py
+++ draw_trees.py
@@ -113,14 +113,3 @@ def build_unit_upgrade_graph(unit):
                     for age in tech_ages[tech]:
                         tree.edge(age, tech)
     return tree
-
-#tech_tree = build_tech_graph()
-##tech_tree.format = 'svg'
-##tech_tree.render()
-#unflat = tech_tree.unflatten(stagger=3)
-#unflat.format = 'svg'
-#unflat.render()
-
-upgrade_tree = build_unit_upgrade_graph('UNIT_DESTROYER')
-upgrade_tree.format = 'svg'
-upgrade_tree.render()
---
+++ local_run.py
@@ -0,0 +1,12 @@
+from draw_trees import *
+
+#tech_tree = build_tech_graph()
+##tech_tree.format = 'svg'
+##tech_tree.render()
+#unflat = tech_tree.unflatten(stagger=3)
+#unflat.format = 'svg'
+#unflat.render()
+
+upgrade_tree = build_unit_upgrade_graph('UNIT_DESTROYER')
+upgrade_tree.format = 'svg'
+upgrade_tree.render()
