b9012410aeeb74961ed2975c42de343bc1a2a763
Richard Halstead <rickynumber24@gmail.com>
2025-02-26 15:37:21 -0500

    Create a basic server in server.py

---
+++ server.py
@@ -0,0 +1,32 @@
+from draw_trees import build_unit_upgrade_graph
+from flask import Flask, make_response, request
+
+import os
+
+app = Flask(__name__)
+
+@app.route("/upgrade_tree.svg", methods=["GET"])
+def index():
+    """
+    returns an SVG of the upgrade tree and relevant techs in an HTTP response.
+    """
+    try:
+        graph = build_unit_upgrade_graph(request.args.get("entity"))
+        graph.format = "svg"
+        image = graph.pipe()
+        response = make_response(image)
+        response.headers.set("Content-Type", "image/svg")
+        return response
+
+    except Exception as e:
+        print(f"error: {e}")
+
+        return "Internal Server Error", 500
+
+
+if __name__ == "__main__":
+    PORT = int(os.getenv("PORT")) if os.getenv("PORT") else 8080
+
+    # This is used when running locally. Gunicorn is used to run the
+    # application on Cloud Run. See entrypoint in Dockerfile.
+    app.run(host="127.0.0.1", port=PORT, debug=True)
